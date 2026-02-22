fb4b083b1ebe57e167e1375354160bd888e9390f
유기현 <gihyeon9412@gmail.com>
2026-02-22 19:02:29 +0900

    화면 등록 기능 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -27,6 +27,7 @@ public class HelloController {
     }
 
     @GetMapping("hello-api")
+
     @ResponseBody
     public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
